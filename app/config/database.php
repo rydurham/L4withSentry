f4f97d744d662541bca9479d355958eabf78ab09
Ryan C. Durham <rydurham@gmail.com>
2013-02-18 12:19:01 -0800

    Revert "Update Database Config"
    This reverts commit fa0c9f554993cc8d36360f1ee09dcb0708e4f860.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'l4sentry',
+			'database'  => 'database',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
