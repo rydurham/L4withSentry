5c4802fd8feab476e933addf25898725088f2a60
Ryan C. Durham <rydurham@gmail.com>
2013-02-18 12:07:13 -0800

    Added Sentry 2
    As per https://github.com/cartalyst/sentry

--- app/config/app.php
+++ app/config/app.php
@@ -109,6 +109,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
@@ -172,6 +173,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Sentry'		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry'
 
 	),
 
