fa0c9f554993cc8d36360f1ee09dcb0708e4f860
Ryan C. Durham <rydurham@gmail.com>
2013-02-18 12:17:59 -0800

    Update Database Config

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'l4sentry',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
