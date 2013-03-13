1327ec813be4d5dcbf84d992e3d1c15aadc76eaa
Ryan C. Durham <rydurham@gmail.com>
2013-03-12 17:28:42 -0700

    Show Group View

---
+++ app/views/groups/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+
+{{-- Web site Title --}}
+@section('title')
+@parent
+View Group
+@stop
+
+{{-- Content --}}
+@section('content')
+<div class="span10 well">
+	<h1>{{ $group['name'] }} </h1>
+    <p>Permsissions:
+        <br /> 
+        {{ var_dump($groupPermissions) }}</p>
+</div>
+
+@stop
