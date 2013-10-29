1ddb61491d19a7d15ac47356539ea264ebdcd293
Ryan C. Durham <rydurham@gmail.com>
2013-10-29 16:18:11 -0700

    Add Way/Generators

--- app/config/app.php
+++ app/config/app.php
@@ -113,7 +113,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Cartalyst\Sentry\SentryServiceProvider',
-
+		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
