e3da7a86188376272db1f6e20730b3f75a4d46b5
Ryan Durham <rydurham@gmail.com>
2013-11-11 16:31:37 -0800

    GroupForm

---
+++ app/Authority/Service/Form/Group/GroupFormLaravelValidator.php
@@ -0,0 +1,24 @@
+<?php namespace Authority\Service\Form\Login;
+
+use Authority\Service\Validation\AbstractLaravelValidator;
+
+class LoginFormLaravelValidator extends AbstractLaravelValidator {
+	
+	/**
+	 * Validation rules
+	 *
+	 * @var Array 
+	 */
+	protected $rules = array(
+		'name' => 'required|min:4'
+	);
+
+	/**
+	 * Custom Validation Messages
+	 *
+	 * @var Array 
+	 */
+	protected $messages = array(
+		//'email.required' => 'An email address is required.'
+	);
+}
