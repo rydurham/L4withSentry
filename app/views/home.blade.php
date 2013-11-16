a9946de09bcdcfff2723466f6689991fc6aacf4c
Ryan Durham <rydurham@gmail.com>
2013-11-16 15:20:47 -0800

    Added Glyphicon Hafling assets. Closes #25

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -19,7 +19,7 @@ Hello World
 @if (Sentry::check() )
 	<div class="panel panel-success">
 		 <div class="panel-heading">
-			<h3 class="panel-title"><span class="glyphicon glyphicon-pushpin">You are currently logged in.</h3>
+			<h3 class="panel-title"><span class="glyphicon glyphicon-ok"></span> You are currently logged in.</h3>
 		</div>
 		<div class="panel-body">
 			<p><strong>Session Data:</strong></p>
