542b79d509491da7beec3a5afc782fc6ed97feec
Ryan Durham <rydurham@gmail.com>
2013-11-16 20:13:15 -0800

    Start localization for views

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 {{-- Web site Title --}}
 @section('title')
 @parent
-Hello World
+{{trans('pages.helloworld')}}
 @stop
 
 {{-- Content --}}
@@ -11,18 +11,18 @@ Hello World
 
 <div class="jumbotron">
   <div class="container">
-    <h1>Hello, world!</h1>
-    <p>This is an example of <a href="https://github.com/laravel/laravel/tree/develop">Laravel 4</a> running with <a href="https://github.com/cartalyst/sentry">Sentry 2.0</a> and <a href="http://getbootstrap.com/">Bootstrap 3.0</a>.</p>
+    <h1>{{trans('pages.helloworld')}}</h1>
+    <p>{{trans('pages.description')}}</p>
   </div>
 </div>
 
 @if (Sentry::check() )
 	<div class="panel panel-success">
 		 <div class="panel-heading">
-			<h3 class="panel-title"><span class="glyphicon glyphicon-ok"></span> You are currently logged in.</h3>
+			<h3 class="panel-title"><span class="glyphicon glyphicon-ok"></span> {{trans('pages.loginstatus')}}</h3>
 		</div>
 		<div class="panel-body">
-			<p><strong>Session Data:</strong></p>
+			<p><strong>{{trans('pages.sessiondata')}}:</strong></p>
 			<pre>{{ var_dump(Session::all()) }}</pre>
 		</div>
 	</div>
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -3,40 +3,65 @@
 {{-- Web site Title --}}
 @section('title')
 @parent
-Home
+{{trans('pages.users')}}
 @stop
 
 {{-- Content --}}
 @section('content')
-<h4>Current Users:</h4>
+<h4>{{trans('pages.currentusers')}}:</h4>
 <div class="row">
   <div class="col-md-10 col-md-offset-1">
 	<div class="table-responsive">
 		<table class="table table-striped table-hover">
 			<thead>
-				<th>User</th>
-				<th>Status</th>
-				<th>Options</th>
+				<th>{{trans('pages.user')}}</th>
+				<th>{{trans('pages.status')}}</th>
+				<th>{{trans('pages.options')}}</th>
 			</thead>
 			<tbody>
 				@foreach ($users as $user)
 					<tr>
 						<td><a href="{{ action('UserController@show', array($user->id)) }}">{{ $user->email }}</a></td>
-						<td>{{ $user->status }} </td>
 						<td>
-							<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@edit', array($user->id)) }}'">Edit</button> 
+							<?php
+								switch ($user->status) 
+								{
+									case 'Active':
+									echo trans('pages.active');
+									break;
+
+									case 'Not Active':
+									echo trans('pages.notactive');
+									break;
+
+									case 'Suspended':
+									echo trans('pages.suspended');
+									break;
+
+									case 'Banned':
+									echo trans('pages.banned');
+									break;
+
+									default:
+
+									break;
+								} 
+							?>
+						</td>
+						<td>
+							<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@edit', array($user->id)) }}'">{{trans('pages.actionedit')}}</button> 
 							@if ($user->status != 'Suspended')
-								<button class="btn btn-default" type="button" onClick="location.href='{{ route('suspendUserForm', array($user->id)) }}'">Suspend</button> 
+								<button class="btn btn-default" type="button" onClick="location.href='{{ route('suspendUserForm', array($user->id)) }}'">{{trans('pages.actionsuspend')}}</button> 
 							@else
-								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@unsuspend', array($user->id)) }}'">Un-Suspend</button> 
+								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@unsuspend', array($user->id)) }}'">{{trans('pages.actionunsuspend')}}</button> 
 							@endif
 							@if ($user->status != 'Banned')
-								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@ban', array($user->id)) }}'">Ban</button> 
+								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@ban', array($user->id)) }}'">{{trans('pages.actionban')}}</button> 
 							@else
-								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@unban', array($user->id)) }}'">Un-Ban</button> 
+								<button class="btn btn-default" type="button" onClick="location.href='{{ action('UserController@unban', array($user->id)) }}'">{{trans('pages.actionunban')}}</button> 
 							@endif
 							
-							<button class="btn btn-default action_confirm" href="{{ action('UserController@destroy', array($user->id)) }}" data-token="{{ Session::getToken() }}" data-method="delete">Delete</button></td>
+							<button class="btn btn-default action_confirm" href="{{ action('UserController@destroy', array($user->id)) }}" data-token="{{ Session::getToken() }}" data-method="delete">{{trans('pages.actiondelete')}}</button></td>
 					</tr>
 				@endforeach
 			</tbody>
