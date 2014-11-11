da8ef0d4ccc84d417c6854a0b6a137d4a587287c
Ryan Durham <ryan@stagerightlabs.com>
2014-11-11 10:11:31 -0800

    Add additional options to mail config

--- app/config/mail.php
+++ app/config/mail.php
@@ -94,4 +94,28 @@ return array(
 
 	'password' => null,
 
+	/*
+	|--------------------------------------------------------------------------
+	| Sendmail System Path
+	|--------------------------------------------------------------------------
+	|
+	| When using the "sendmail" driver to send e-mails, we will need to know
+	| the path to where Sendmail lives on this server. A default path has
+	| been provided here, which will work well on most of your systems.
+	|
+	*/
+	'sendmail' => '/usr/sbin/sendmail -bs',
+	/*
+	|--------------------------------------------------------------------------
+	| Mail "Pretend"
+	|--------------------------------------------------------------------------
+	|
+	| When this option is enabled, e-mail will not actually be sent over the
+	| web and will instead be written to your application's logs files so
+	| you may inspect the message. This is great for local development.
+	|
+	*/
+	'pretend' => false,
+
+
 );
